f73acc4cbf99f8c3022c87f5a8560a71c6b48450
cameron <physicsthacker@gmail.com>
2026-04-13 16:16:09 -0700

    fix lazy loading for markdown preview

--- config/nvim/lua/plugins/markdown-preview.lua
+++ config/nvim/lua/plugins/markdown-preview.lua
@@ -2,8 +2,12 @@ return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  build = function() vim.fn["mkdp#util#install"]() end,
+  build = function()
+    require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+    vim.fn["mkdp#util#install"]()
+  end,
   config = function()
     vim.g.mkdp_echo_preview_url = 1
-  end
+    vim.cmd([[do FileType]])
+  end,
 }
